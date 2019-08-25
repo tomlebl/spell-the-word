69076d603136af5904918666b2d5f01c39ffa18f
Tomas Lebl <tomlebl44@gmail.com>
2019-08-25 23:39:04 +0100

    Initial files

---
+++ src/hangman.js
@@ -0,0 +1,62 @@
+class Hangman {
+  constructor(word, guesses) {
+    this.word = word.toLowerCase().split('')
+    this.remainingGuesses = guesses
+    this.guessedLetters = []
+    this.wordToGuess = word.toLowerCase()
+    this.status = 'playing'
+  }
+  makeGuess(guess) {
+    guess = guess.toLowerCase()
+    const isUnique = !this.guessedLetters.includes(guess)
+    const isBadGuess = !this.word.includes(guess)
+
+    if (this.status !== 'playing') {
+      return
+    }
+
+    if (isUnique) {
+      //this.guessedLetters.push(guess)
+      this.guessedLetters = [...this.guessedLetters, guess]
+    }
+
+    if (isUnique && isBadGuess) {
+      this.remainingGuesses--
+    }
+
+    this.checkStatus()
+  }
+  get statusMessage() {
+    if (this.status === 'finished') {
+      return 'Great work! You guessed the word.'
+    } else if (this.status === 'failed') {
+      return `Nice try! The word was "${this.wordToGuess}".`
+    } else {
+      return `You have ${this.remainingGuesses} remaining guesses`
+    }
+  }
+  get puzzle() {
+    let puzzle = ''
+    this.word.forEach(letter => {
+      if (this.guessedLetters.includes(letter) || letter === ' ') {
+        puzzle += letter
+      } else {
+        puzzle += '*'
+      }
+    })
+
+    return puzzle
+  }
+
+  checkStatus() {
+    if (this.remainingGuesses === 0) {
+      this.status = 'failed'
+    } else if (this.puzzle === this.wordToGuess) {
+      this.status = 'finished'
+    } else {
+      this.status = 'playing'
+    }
+  }
+}
+
+export { Hangman as default }
---
+++ src/index.js
@@ -0,0 +1,75 @@
+import Hangman from './hangman'
+import getPuzzle from './requests'
+
+const puzzleEl = document.querySelector('#puzzle')
+const statusEl = document.querySelector('#status')
+const body = document.querySelector('body')
+const imgConstainer = document.querySelector('#container_img')
+const picture = document.createElement('img')
+
+let puzzle1
+let score = 0
+let guesses = 0
+
+const setRandomColour = () =>
+  '#' + (Math.random().toString(16) + '000000').slice(2, 8).slice(-6)
+
+const setGradient = () => {
+  body.style.background = `linear-gradient(to right, ${setRandomColour()}, ${setRandomColour()})`
+}
+
+const renderImg = imgSrc => {
+  picture.setAttribute('src', imgSrc)
+  imgConstainer.appendChild(picture)
+}
+
+const render = () => {
+  puzzleEl.innerHTML = ''
+  statusEl.textContent = puzzle1.statusMessage
+  puzzle1.puzzle.split('').forEach(element => {
+    let letter = document.createElement('span')
+    letter.textContent = element
+    puzzleEl.appendChild(letter)
+  })
+}
+
+const getDifficulty = () => {
+  if (document.getElementById('beginner').checked) {
+    return 1
+  } else if (document.getElementById('intermediate').checked) {
+    return 2
+  } else if (document.getElementById('advanced').checked) {
+    return 3
+  }
+}
+
+const startPuzzle = () => {
+  const { word, imgSrc } = getPuzzle(getDifficulty())
+  puzzle1 = new Hangman(word, guesses)
+  setGradient()
+  renderImg(imgSrc)
+  render()
+}
+
+const startGame = () => {
+  guesses = 6 - getDifficulty()
+  score = 0
+  startPuzzle()
+}
+
+window.addEventListener('keypress', e => {
+  const guess = String.fromCharCode(e.charCode)
+  puzzle1.makeGuess(guess)
+  if (puzzle1.status === 'finished') {
+    score = score + getDifficulty()
+    guesses = puzzle1.remainingGuesses
+
+    setTimeout(console.log(score), 50000)
+    startPuzzle()
+  }
+  render()
+})
+
+document.querySelector('#reset').addEventListener('click', startGame)
+
+//startPuzzle()
