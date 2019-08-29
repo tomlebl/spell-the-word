fedd48f2ac1fd61f0765f4f80664ec18bc28e13a
Tomas Lebl <tl12@st-andrews.ac.uk>
2019-08-29 23:47:40 +0100

    Select difficulty feature added

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import Hangman from './hangman'
 import getPuzzle from './requests'
 
-const puzzleEl = document.querySelector('#puzzle')
 const statusEl = document.querySelector('#status')
 const body = document.querySelector('body')
+const difficultyEl = document.querySelector('#difficulty')
 const imgConstainer = document.querySelector('#container_img')
 const picture = document.createElement('img')
+const puzzleDisplay = document.createElement('div')
 
 let puzzle1
 let score = 0
 let guesses = 0
+let difficulty
 
 const setRandomColour = () =>
   '#' + (Math.random().toString(16) + '000000').slice(2, 8).slice(-6)
@@ -24,8 +26,9 @@ const renderImg = imgSrc => {
 }
 
 const render = () => {
+  const puzzleEl = document.querySelector('#puzzle')
   puzzleEl.innerHTML = ''
-  statusEl.textContent = puzzle1.statusMessage
+  //statusEl.textContent = puzzle1.statusMessage
   puzzle1.puzzle.split('').forEach(element => {
     let letter = document.createElement('span')
     letter.textContent = element
@@ -44,7 +47,7 @@ const getDifficulty = () => {
 }
 
 const startPuzzle = () => {
-  const { word, imgSrc } = getPuzzle(getDifficulty())
+  const { word, imgSrc } = getPuzzle(difficulty)
   puzzle1 = new Hangman(word, guesses)
   setGradient()
   renderImg(imgSrc)
@@ -54,15 +57,19 @@ const startPuzzle = () => {
 const startGame = () => {
   guesses = 6 - getDifficulty()
   score = 0
+  difficulty = getDifficulty()
+  puzzleDisplay.innerHTML = '<div id="puzzle" class="puzzle">'
+  puzzleDisplay.classList.add('display')
+  console.log(puzzleDisplay)
+  difficultyEl.replaceWith(puzzleDisplay)
   startPuzzle()
-  console.log('startGame', puzzle1.statusMessage)
 }
 
 window.addEventListener('keypress', e => {
   const guess = String.fromCharCode(e.charCode)
   puzzle1.makeGuess(guess)
   if (puzzle1.status === 'finished') {
-    score = score + getDifficulty()
+    score = score + difficulty
     guesses = puzzle1.remainingGuesses
 
     setTimeout(console.log(score), 50000)
