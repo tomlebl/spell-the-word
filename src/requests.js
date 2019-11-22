32de8223bf1466b63080295ee82679318bdd8f2d
Tomas Lebl <tomlebl44@gmail.com>
2019-11-22 23:21:56 +0000

    loadImg function + Webpack config for 2 pages

--- src/requests.js
+++ src/requests.js
@@ -1,30 +1,40 @@
 import puzzles from './puzzles'
 
 const getPuzzle = (difficulty, usedWords) => {
-  const puzzlesFiltered = puzzles
-    .filter(puzzle => puzzle.difficulty === difficulty)
-    .filter(puzzle => !usedWords.includes(puzzle.word))
+	const puzzlesFiltered = puzzles
+		.filter(puzzle => puzzle.difficulty === difficulty)
+		.filter(puzzle => !usedWords.includes(puzzle.word))
 
-  return puzzlesFiltered.length === 0
-    ? { word: '' }
-    : puzzlesFiltered[Math.floor(Math.random() * puzzlesFiltered.length)]
+	return puzzlesFiltered.length === 0
+		? { word: '' }
+		: puzzlesFiltered[Math.floor(Math.random() * puzzlesFiltered.length)]
 }
 
 const saveHiScore = score => {
-  localStorage.setItem('hiScore', JSON.stringify(score))
+	localStorage.setItem('hiScore', JSON.stringify(score))
 }
 
 const loadHiScore = () => {
-  const scoreJSON = localStorage.getItem('hiScore')
+	const scoreJSON = localStorage.getItem('hiScore')
 
-  return scoreJSON ? JSON.parse(scoreJSON) : 0
+	return scoreJSON ? JSON.parse(scoreJSON) : 0
 }
 
 const getPuzzleCount = difficulty => {
-  const puzzleArrayFiltered = puzzles.filter(
-    puzzle => puzzle.difficulty === difficulty
-  )
-  return puzzleArrayFiltered.length
+	const puzzleArrayFiltered = puzzles.filter(puzzle => puzzle.difficulty === difficulty)
+	return puzzleArrayFiltered.length
 }
 
-export { getPuzzle, saveHiScore, loadHiScore, getPuzzleCount }
+const loadImg = imgSrc =>
+	new Promise((resolve, reject) => {
+		const image = new Image()
+		image.addEventListener('load', () => {
+			resolve(image)
+		})
+		image.addEventListener('error', () => {
+			reject('Image did not load. Invalid source')
+		})
+		image.src = imgSrc
+	})
+
+export { getPuzzle, saveHiScore, loadHiScore, getPuzzleCount, loadImg }
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,31 @@
 const path = require('path')
 
 module.exports = {
-    entry: ['babel-polyfill', './src/index.js'],
-    output: {
-        path: path.resolve(__dirname, 'public/scripts'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['env']
-                }
-            }
-        }]
-    },
-    devServer: {
-        contentBase: path.resolve(__dirname, 'public'),
-        publicPath: '/scripts/'
-    },
-    devtool: 'source-map'
+	entry: {
+		index: ['babel-polyfill', './src/index.js'],
+		add: ['babel-polyfill', './src/add.js']
+	},
+	output: {
+		path: path.resolve(__dirname, 'public/scripts'),
+		filename: '[name]-bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['env']
+					}
+				}
+			}
+		]
+	},
+	devServer: {
+		contentBase: path.resolve(__dirname, 'public'),
+		publicPath: '/scripts/'
+	},
+	devtool: 'source-map'
 }
